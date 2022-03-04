e91ac8eae805c0bf1193d00efab9a059cdb27fdf
Isaiah Henson <Devpuddles@gmail.com>
2022-03-04 16:16:03 -0500

    super-review practice

---
+++ js11/main.js
@@ -0,0 +1,19 @@
+// Create a function that takes in an array. If the first number, is less than the last number, alert "Hi". If the first number is greater than the last number, alert "Bye". If they are equal, alert "We close in an hour".
+
+function nums (arr){
+    if(arr[0] < arr.length[i]){
+        console.log('hi')
+    }
+}
+
+function array(arry) {
+    if(arry[0] < arry[arry.length - 1]) {
+        alert('Hi')
+    } else if(arry[0] > arry[arry.length - 1]){
+        alert('Bye')
+    }else {
+        alert('We close in an hour')
+    }
+}
+
+array([1,2,3,4,5,])
