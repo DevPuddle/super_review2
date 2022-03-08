c14440bbd3ee64c984440d381664e3edf1914bb3
Isaiah Henson <Devpuddles@gmail.com>
2022-03-08 16:22:56 -0500

    super-review practice

---
+++ js16/main.js
@@ -0,0 +1,12 @@
+//Write your pseduo code first! 
+//c to f
+//degree in c
+//convertion formula
+
+
+function celToFahr(cel){
+    let convert = (cel * 9/5) + 32
+    console.log(convert)
+}
+
+celToFahr(10)
